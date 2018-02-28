f4c4ed5b14e451c013211926737ec872d82af54c
Charles <charlescollings@usa.net>
2018-02-27 23:33:13 -0600

    feb27_final

---
+++ assets/Javascript/game.js
@@ -0,0 +1,15 @@
+// connect JS variables to document
+let press = document.getElementById("pressDiv");
+let wins = document.getElementById("winsDiv");
+let currentWord = document.getElementById("currentWordDiv");
+let numberGuesses = document.getElementById("numberGuessesDiv");
+let lettersGuessed = document.getElementById("lettersGuessedDiv");
+
+// make an array of 5 words to be used for the game
+let gameWords = ["bonham", "pert", "moon", "grohl", "krupa"]
+
+
+// make an array of 26 letters of alphabet
+
+
+
