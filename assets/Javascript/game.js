2c8250e73a5853dc222a60c98eb4c8d0db3610e6
Charles <charlescollings@usa.net>
2018-02-28 11:20:59 -0600

    WedMornPush

--- assets/Javascript/game.js
+++ assets/Javascript/game.js
@@ -5,6 +5,16 @@ let currentWord = document.getElementById("currentWordDiv");
 let numberGuesses = document.getElementById("numberGuessesDiv");
 let lettersGuessed = document.getElementById("lettersGuessedDiv");
 
+// Set currentWins to 0 to start
+let currentWins = 0;
+
+press = "Press any key to start!"
+press.textContent = (press);
+
+
+// Set up page that will show on load
+wins.textContent = ("wins: " + currentWins);
+
 // make an array of 5 words to be used for the game
 let gameWords = ["bonham", "pert", "moon", "grohl", "krupa"]
 
